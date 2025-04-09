65bbf53e417d785207e527ffe99411b2e9464768
LordWLLM <ville.gustav@protonmail.com>
2025-04-09 19:18:51 +0200

    bruh

--- Flight/src/main.cpp
+++ Flight/src/main.cpp
@@ -4,21 +4,18 @@
 SingleWireSerial mySerial(false);
 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup()
 {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  mySerial.begin(9600);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  mySerial.write(0xA0);
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
-}
